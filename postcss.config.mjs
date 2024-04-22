0c4a710bb47a8a7dd8b4461e01b6dc505df91f9b
Thomas Lepérou <thomas.leperou@gmail.com>
2024-04-22 23:30:06 +1000

    feat(docs): 🚀 integrate mobile

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,19 @@
 import autoprefixer from 'autoprefixer'
 import postcssPresetEnv from 'postcss-preset-env'
 import csso from 'postcss-csso'
+import postcssGlobals from '@csstools/postcss-global-data'
 
 
 export default {
 	plugins: [
+		postcssGlobals({
+			files: [
+				'./src/finess/props/media/media.breakpoints.css',
+				'./src/finess/props/media/media.devices.css',
+				'./src/finess/props/media/media.prefers.css',
+				'./src/finess/icons.css',
+			]
+		}),
 		postcssPresetEnv({
 			stage: 3,
 			features: {
